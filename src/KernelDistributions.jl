625c3570e2154cba31747c341a5e330b33302d4f
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-12-28 20:42:27 +0100

    Remove greet

--- src/KernelDistributions.jl
+++ src/KernelDistributions.jl
@@ -27,7 +27,6 @@ Most of the time Float64 precision is not required, especially for GPU computati
 Thus, I default to Float32, mostly for memory capacity reasons.
 """
 module KernelDistributions
-greet() = print("KernelDistributions.jl greetings from Tim.")
 
 using Bijectors
 using CUDA
