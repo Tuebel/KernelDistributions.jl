f6141d00e62b15627da6786cb1c08f0835b25e84
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-09-19 16:04:47 +0200

    Add plots for classification probabilities

--- scripts/plot_distributions.jl
+++ scripts/plot_distributions.jl
@@ -47,7 +47,8 @@ diss_defaults()
 μ = 1.0
 σ = 0.1
 β = 1.0
-z_max = 2.0
+z_min = 0.5
+z_max = 1.5
 z = 0:0.01:(z_max+0.2)
 w_tail = 5
 normal = KernelNormal(μ, σ)
@@ -63,8 +64,8 @@ pdf_truncated = pdf.(truncated_exponential, z)
 pdf_uniform = 0.5 .* pdf.(uniform, z)
 
 fig = MK.Figure(resolution=(DISS_WIDTH, 2 / 5 * DISS_WIDTH))
-ax1 = MK.Axis(fig[1, 1]; title="Exponential", xlabel="depth / m", ylabel="probability density")
-ax2 = MK.Axis(fig[1, 2]; title="Mixture", xlabel="depth / m", limits=((0, nothing), nothing))
+ax1 = MK.Axis(fig[1, 1]; title="Exponential", xlabel="depth / m", ylabel="probability density", limits=((0, nothing), (0, nothing)))
+ax2 = MK.Axis(fig[1, 2]; title="Mixture", xlabel="depth / m", limits=((0, nothing), (0, nothing)))
 
 MK.lines!(ax1, z, w_tail * pdf_uniform; label="only uniform", visible=false)
 MK.lines!(ax1, z, pdf_exponential; label="unmodified exponential")
@@ -80,4 +81,37 @@ MK.vspan!(ax2, 0.0, μ)
 MK.Legend(fig[2, :], ax2; orientation=:horizontal)
 
 MK.save("pixel_likelihoods.pdf", fig)
+fig
+
+# different classification probabilities for smooth
+fig = MK.Figure(resolution=(DISS_WIDTH, 2 / 5 * DISS_WIDTH))
+ax = MK.Axis(fig[1, 1]; xlabel="depth / m", ylabel="probability density", limits=((0, nothing), (0, nothing)))
+for o in 0.2:0.2:0.8
+    MK.lines!(ax, z, o * pdf_normal + (1 - o) * (pdf_smooth + pdf_uniform); label="o = $o")
+end
+MK.Legend(fig[:, 2], ax)
+
+MK.save("pixel_likelihoods_o_smooth.pdf", fig)
+fig
+
+# different classification probabilities for exponential
+fig = MK.Figure(resolution=(DISS_WIDTH, 2 / 5 * DISS_WIDTH))
+ax = MK.Axis(fig[1, 1]; xlabel="depth / m", ylabel="probability density", limits=((0, nothing), (0, nothing)))
+for o in 0.2:0.2:0.8
+    MK.lines!(ax, z, o * pdf_normal + (1 - o) * (pdf_exponential + pdf_uniform); label="o = $o")
+end
+MK.Legend(fig[:, 2], ax)
+
+MK.save("pixel_likelihoods_o_unmodified.pdf", fig)
+fig
+
+# different classification probabilities for truncated
+fig = MK.Figure(resolution=(DISS_WIDTH, 2 / 5 * DISS_WIDTH))
+ax = MK.Axis(fig[1, 1]; xlabel="depth / m", ylabel="probability density", limits=((0, nothing), (0, nothing)))
+for o in 0.2:0.2:0.8
+    MK.lines!(ax, z, o * pdf_normal + (1 - o) * (pdf_truncated + pdf_uniform); label="o = $o")
+end
+MK.Legend(fig[:, 2], ax)
+
+MK.save("pixel_likelihoods_o_truncated.pdf", fig)
 fig
