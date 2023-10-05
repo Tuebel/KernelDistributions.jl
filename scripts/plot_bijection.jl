2dfd64e80410f19fe358a596b2c3f64c628f6d95
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-10-05 22:13:09 +0200

    Fix plot title

--- scripts/plot_bijection.jl
+++ scripts/plot_bijection.jl
@@ -56,7 +56,7 @@ x = exp.(y)
 
 fig = MK.Figure(resolution=(DISS_WIDTH, 1 / 4 * DISS_WIDTH))
 ax1 = MK.Axis(fig[1, 1]; title="y ~ 𝓝 (1,1)", ylabel="probability density")
-ax2 = MK.Axis(fig[1, 2]; title="x=log(y)")
+ax2 = MK.Axis(fig[1, 2]; title="x=b⁻¹(y)=exp(y)")
 MK.density!(ax1, y)
 MK.density!(ax2, x)
 MK.save("transform_density.pdf", fig)
